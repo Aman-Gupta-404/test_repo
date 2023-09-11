643188d3ed97b6e53332b3f8f8d9de768c615708
Aman-Gupta-404 <ag9139563@gmail.com>
2023-09-11 11:55:12 +0530

    mistake commit

--- files/test.js
+++ files/test.js
@@ -1,3 +1,5 @@
 console.log("this is the second commit");
 
 console.log("third commit!");
+
+console.log("this is the unwanted commit!");
--- index.js
+++ index.js
@@ -1,3 +1,5 @@
 console.log("the first change ever!");
 
 console.log("this is the third commit ");
+
+console.log("good commit along with the unwanted commit");
